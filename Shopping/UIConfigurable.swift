117519aaeb1b9a7d9e6d780333859ef4cdf9b159
moklad <dev.moklad@gmail.com>
2025-07-25 17:44:49 +0900

    feat: UI 구조를 잡기 위한 프로토콜 추가

---
+++ Shopping/UIConfigurable.swift
@@ -0,0 +1,14 @@
+//
+//  ViewDesignProtocol.swift
+//  Shopping
+//
+//  Created by Youngjun Kim on 7/25/25.
+//
+
+import Foundation
+
+protocol UIConfigurable {
+    func configureHierarchy()
+    func configureLayout()
+    func configureView()
+}
