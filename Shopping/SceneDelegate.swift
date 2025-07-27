6f8bec1bf7733192479444793a92b109eebb3b3b
moklad <dev.moklad@gmail.com>
2025-07-27 14:01:59 +0900

    feat: NavgiationBar 에 backButton 색상 변경을 위한 tintcolor 설정

--- Shopping/SceneDelegate.swift
+++ Shopping/SceneDelegate.swift
@@ -19,6 +19,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         
+        NavConfigurtor.configureUI()
+        
         let rootVC = ViewController()
         let navigationController = UINavigationController(rootViewController: rootVC)
         
