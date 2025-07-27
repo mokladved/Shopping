2211a92bd989e044a2f9a96d318e7aead2e9312b
moklad <dev.moklad@gmail.com>
2025-07-27 17:52:02 +0900

    feat: 네트워크 통신 응답 데이터를 객체로 변환하기 위한 모델 정의

---
+++ Shopping/Resources/ShopItem.swift
@@ -0,0 +1,20 @@
+//
+//  ShopItem.swift
+//  Shopping
+//
+//  Created by Youngjun Kim on 7/27/25.
+//
+
+import Foundation
+
+struct ShopItem: Decodable {
+    let total: Int
+    let items: [Item]
+}
+
+struct Item: Decodable {
+    let title: String
+    let imageUrl: String
+    let mallName: String
+    let lprice: String
+}
