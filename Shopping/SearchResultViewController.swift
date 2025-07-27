556b4b945d09d599763889aa2c3430663dbd0ad3
moklad <dev.moklad@gmail.com>
2025-07-28 01:06:19 +0900

    feat(Search): 검색 결과를 위한 UI 기본 설정
    - UICollectionView를 사용
    - 동적으로 셀이 차지하는 크기 계산을 위해 FlowLayout 프로토콜 채택
    - UIConfigurable 프로토콜 채택하여 코드 역할 분리

--- Shopping/SearchResultViewController.swift
+++ Shopping/SearchResultViewController.swift
@@ -8,14 +8,57 @@
 import UIKit
 
 class SearchResultViewController: UIViewController {
-
-//    var data: Item
+    var shoppingItems: [Item] = []
+    var keyword: String?
     
+    lazy var collectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = UIConstants.spacing * 2
+        layout.minimumInteritemSpacing = UIConstants.spacing
+
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+            
+        collectionView.delegate = self
+        collectionView.dataSource = self
+            
+        collectionView.register(SearchResultCollectionViewCell.self, forCellWithReuseIdentifier: SearchResultCollectionViewCell.identifier)
+        
+        return collectionView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
+}
+
+extension SearchResultViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 0, height: 0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SearchResultCollectionViewCell.identifier, for: indexPath) as! SearchResultCollectionViewCell
+        return cell
+    }
+}
+
+extension SearchResultViewController: UIConfigurable {
+    func configureHierarchy() {
+        
+    }
+    
+    func configureLayout() {
+        
+    }
+    
+    func configureView() {
+        view.backgroundColor = .black
+        navigationItem.title = keyword
+    }
 }
