83a2aeda18854e81543ff3ec070590b5de931b5f
moklad <dev.moklad@gmail.com>
2025-07-27 21:57:20 +0900

    feat: 검색 결과를 보여주기 위한 셀 기본 UI 구현

---
+++ Shopping/SearchResultViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ResultViewController.swift
+//  Shopping
+//
+//  Created by Youngjun Kim on 7/27/25.
+//
+
+import UIKit
+
+class SearchResultViewController: UIViewController {
+
+//    var data: Item
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
