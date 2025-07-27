204a3a5ddaf79004fd8ac91531ab65fcd6cde2ab
moklad <dev.moklad@gmail.com>
2025-07-28 01:09:56 +0900

    feat: CollectionViewCell의 identifier 추가

--- Shopping/Title.swift
+++ Shopping/Title.swift
@@ -11,4 +11,5 @@ enum Title {
     static let homeVCImageDescription = "쇼핑하구팡"
     static let navTitle = "영캠러의 쇼핑쇼핑"
     static let placeholder = "브랜드, 상품, 프로필, 태그 등"
+    static let SearchResultCVCIdentifier = "SearchResultCollectionViewCell"
 }
