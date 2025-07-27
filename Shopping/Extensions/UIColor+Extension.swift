0c653bd27a6db640e7a8c02472c59e9ceb4d0237
moklad <dev.moklad@gmail.com>
2025-07-27 16:54:58 +0900

    feat: 커스텀 색상을 Extension을 통해 관리

---
+++ Shopping/Extensions/UIColor+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor+Extension.swift
+//  Shopping
+//
+//  Created by Youngjun Kim on 7/27/25.
+//
+
+import UIKit
+
+extension UIColor {
+    static let myBGDarkGray = UIColor(red: 28/255, green: 28/255, blue: 31/255, alpha: 1.0)
+    static let myFGLightGray = UIColor(red: 152/255, green: 152/255, blue: 160/255, alpha: 1.0)
+}
