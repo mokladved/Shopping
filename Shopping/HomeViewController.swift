8adfed593d263d8fa7f56ee1951e69e5bc230f54
moklad <dev.moklad@gmail.com>
2025-07-27 16:00:56 +0900

    feat(Home): 기본 레이아웃 구현
    - 화면 중앙에 놓일 이미지와 Label을 UIStackView로 구현
    - UIConfigurable을 채택하여 계층 구조 및 UI 설정에 필요한 코드 구현
    - SnapKit 을 활용하여 오토레이아웃 구현

--- Shopping/HomeViewController.swift
+++ Shopping/HomeViewController.swift
@@ -6,30 +6,62 @@
 //
 
 import UIKit
+import Alamofire
+import SnapKit
 
 class HomeViewController: UIViewController {
+    let searchBar = UISearchBar()
+    let stackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 64
+        return stackView
+    }()
+    
+    let backImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(resource: .shoppingMan)
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    let descLabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.text = Title.homeVCImageDescription
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 18, weight: .bold)
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configureHierarchy()
         configureLayout()
-        
+        configureView()
     }
 
 }
 
 extension HomeViewController: UIConfigurable {
     func configureHierarchy() {
-        
+        view.addSubview(stackView)
+        stackView.addArrangedSubview(backImageView)
+        stackView.addArrangedSubview(descLabel)
     }
     
     func configureLayout() {
+        stackView.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview().inset(40)
+            make.centerX.centerY.equalToSuperview()
+            make.size.equalTo(300)
+        }
         
     }
     
     func configureView() {
         view.backgroundColor = .black
+        navigationItem.title = Title.navTitle
     }
-    
-    
 }
+
