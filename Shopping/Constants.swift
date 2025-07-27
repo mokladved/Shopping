879d40664b70587ad2cfcde6bdf368f11835677a
moklad <dev.moklad@gmail.com>
2025-07-27 15:15:36 +0900

    feat: 상수 관리를 위한 구조체 생성

---
+++ Shopping/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Shopping
+//
+//  Created by Youngjun Kim on 7/27/25.
+//
+
+import Foundation
+
+enum Constants {
+    static let homeVCImageDescription = "쇼핑하구팡"
+    static let navTitle = "영캠러의 쇼핑쇼핑"
+}
