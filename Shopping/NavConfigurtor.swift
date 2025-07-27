672634bf19e89b5ff4c23f20399da2a014efa127
moklad <dev.moklad@gmail.com>
2025-07-27 13:59:30 +0900

    feat: NavigationBar 전체에 적용할 UI를 위한 enum 생성

---
+++ Shopping/NavConfigurtor.swift
@@ -0,0 +1,14 @@
+//
+//  NavConfigurtor.swift
+//  Shopping
+//
+//  Created by Youngjun Kim on 7/27/25.
+//
+
+import UIKit
+
+enum NavConfigurtor {
+    static func configureUI() {
+        UINavigationBar.appearance().tintColor = .white
+    }
+}
