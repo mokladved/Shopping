de0e2c24fa31234f601cb6da8c5c3ed4618e0292
moklad <dev.moklad@gmail.com>
2025-07-28 20:08:41 +0900

    feat: 정렬 로직에 사용하기 위한 열거형 정의

---
+++ Shopping/Sorting.swift
@@ -0,0 +1,15 @@
+//
+//  Sorting..swift
+//  Shopping
+//
+//  Created by Youngjun Kim on 7/28/25.
+//
+
+import Foundation
+
+enum Sorting: String {
+    case sim
+    case date
+    case highPrice = "dsc"
+    case lowPrice = "asc"
+}
