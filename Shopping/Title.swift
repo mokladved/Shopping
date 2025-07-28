fda538b41af5bf0079de3b89499637c76474e2fb
moklad <dev.moklad@gmail.com>
2025-07-28 15:28:01 +0900

    feat: 정렬 버튼 타이틀 상수 설정

--- Shopping/Title.swift
+++ Shopping/Title.swift
@@ -12,4 +12,8 @@ enum Title {
     static let navTitle = "영캠러의 쇼핑쇼핑"
     static let placeholder = "브랜드, 상품, 프로필, 태그 등"
     static let SearchResultCVCIdentifier = "SearchResultCollectionViewCell"
+    static let simSortButton = "정확도"
+    static let dateSortButton = "날짜순"
+    static let highPriceSortButton = "가격높은순"
+    static let lowPriceSortButton = "가격낮은순"
 }
