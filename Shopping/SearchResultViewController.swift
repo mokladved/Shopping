2e31e7aa5f3e18aa1e8bae3d227ac35ebbdf8847
moklad <dev.moklad@gmail.com>
2025-07-28 16:25:30 +0900

    feat: 검색 결과 화면 UI 구현
    - 총 검색 결과 개수를 표기하기 위한 레이블 추가
    - 정렬 버튼에 스타일 적용
    - cellForItemAt 메소드에 아이템 동적 크기 계산 결과 반영
    - 하이어라키 및 레이아웃 구현

--- Shopping/SearchResultViewController.swift
+++ Shopping/SearchResultViewController.swift
@@ -18,6 +18,7 @@ class SearchResultViewController: UIViewController {
         layout.minimumInteritemSpacing = UIConstants.spacing
 
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .clear
             
         collectionView.delegate = self
         collectionView.dataSource = self
@@ -27,39 +28,51 @@ class SearchResultViewController: UIViewController {
         return collectionView
     }()
     
-    let viewWrappedStackView = {
-        let view = UIView()
-        return view
+    let countLabel = {
+        let label = UILabel()
+        label.textColor = .systemGreen
+        label.font = .systemFont(ofSize: 14, weight: .bold)
+        label.text = "1111111"
+        return label
     }()
     
     let stackViewWrapeedButton = {
         let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 8
         return stackView
     }()
     
     let simSortButton = {
         let button = UIButton()
+        button.configuration = .unselectedSortButton(title: Title.simSortButton)
         return button
     }()
     
     let dateSortButton = {
         let button = UIButton()
+        button.configuration = .unselectedSortButton(title: Title.dateSortButton)
         return button
     }()
     
     let highPriceSortButton = {
         let button = UIButton()
+        button.configuration = .unselectedSortButton(title: Title.highPriceSortButton)
         return button
     }()
     
     let lowPriceSortButton = {
         let button = UIButton()
+        button.configuration = .unselectedSortButton(title: Title.lowPriceSortButton)
         return button
     }()
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureHierarchy()
+        configureLayout()
+        configureView()
     }
 
 }
@@ -70,7 +83,9 @@ extension SearchResultViewController: UICollectionViewDelegate, UICollectionView
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 0, height: 0)
+        let width = UIConstants.cellWidth()
+        let height = width * 1.3
+        return CGSize(width: width, height: height)
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -81,16 +96,33 @@ extension SearchResultViewController: UICollectionViewDelegate, UICollectionView
 
 extension SearchResultViewController: UIConfigurable {
     func configureHierarchy() {
-        view.addSubview(viewWrappedStackView)
-        viewWrappedStackView.addSubview(stackViewWrapeedButton)
-        stackViewWrapeedButton.addSubview(simSortButton)
-        stackViewWrapeedButton.addSubview(dateSortButton)
-        stackViewWrapeedButton.addSubview(highPriceSortButton)
-        stackViewWrapeedButton.addSubview(lowPriceSortButton)
+        view.addSubview(countLabel)
+        view.addSubview(collectionView)
+            
+        view.addSubview(stackViewWrapeedButton)
+        
+        stackViewWrapeedButton.addArrangedSubview(simSortButton)
+        stackViewWrapeedButton.addArrangedSubview(dateSortButton)
+        stackViewWrapeedButton.addArrangedSubview(highPriceSortButton)
+        stackViewWrapeedButton.addArrangedSubview(lowPriceSortButton)
     }
     
     func configureLayout() {
+        countLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(16)
+            make.leading.equalTo(view.safeAreaLayoutGuide).inset(16)
+        }
         
+        stackViewWrapeedButton.snp.makeConstraints { make in
+            make.top.equalTo(countLabel.snp.bottom).offset(8)
+            make.leading.equalTo(view.safeAreaLayoutGuide).inset(16)
+            make.height.equalTo(36)
+        }
+
+        collectionView.snp.makeConstraints { make in
+            make.top.equalTo(stackViewWrapeedButton.snp.bottom).offset(16)
+            make.horizontalEdges.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
     }
     
     func configureView() {
