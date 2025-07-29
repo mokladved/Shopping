44eaa4880258724d7ad58509e7fbaff3e0663252
moklad <dev.moklad@gmail.com>
2025-07-29 23:22:15 +0900

    refactor: 상수를 관리하기 위해 하나의 enum 으로 통합
    - Title, UI, API 관련 상수들을 Constants enum으로 관리

---
+++ Shopping/Constants.swift
@@ -0,0 +1,36 @@
+//
+//  UIConstants.swift
+//  Shopping
+//
+//  Created by Youngjun Kim on 7/28/25.
+//
+
+import UIKit
+
+enum Constants {
+    enum UI {
+        static let spacing: CGFloat = 12
+        static let spacingForHorizontalView: CGFloat = 4
+        static let columns: CGFloat = 2
+        static func cellWidth() -> CGFloat {
+            let deviceWidth = UIScreen.main.bounds.width
+            return (deviceWidth - spacing * (columns + 1)) / columns
+        }
+    }
+    enum Title {
+        static let homeVCImageDescription = "쇼핑하구팡"
+        static let navTitle = "영캠러의 쇼핑쇼핑"
+        static let placeholder = "브랜드, 상품, 프로필, 태그 등"
+        static let SearchResultCVCIdentifier = "SearchResultCollectionViewCell"
+        static let simSortButton = "정확도"
+        static let dateSortButton = "날짜순"
+        static let highPriceSortButton = "가격높은순"
+        static let lowPriceSortButton = "가격낮은순"
+        static let recommendCVCIdentifier = "RecommendCollectionViewCell"
+    }
+    enum API {
+        static let baseURL = "https://openapi.naver.com/"
+        static let path = "v1/search/shop.json"
+        static let paginationStandards = 30
+    }
+}
